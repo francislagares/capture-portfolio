513d0fbc9fcdc22be4d7539a426b3f6593661490
Francis Lagares <francis.lagares@gmail.com>
2021-03-03 21:47:26 +0100

    feat: create nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs';
 
 const App = (): JSX.Element => {
   return (
     <div className='app'>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
