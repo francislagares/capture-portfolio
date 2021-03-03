c5ef3c2429ba80cf62a26cc4ca8f5c43234155ac
Francis Lagares <francis.lagares@gmail.com>
2021-03-03 21:46:09 +0100

    feat: create faq component

---
+++ src/components/FaqSection.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { About } from '../styles';
+
+const Faq = styled(About)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: .2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    width: 100%;
+    height: .2rem;
+    margin: 2rem 0;
+    background: #ccc;
+  }
+
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0;
+
+    p {
+      padding: 1rem 0;
+    }
+  }
+`;
+
+const FaqSection = (): JSX.Element => {
+  return (
+    <Faq>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How do I start ?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing Necessitatibus,
+            neque.
+          </p>
+        </div>
+        <div className='faq-line'></div>
+      </div>
+      <div className='question'>
+        <h4>Daily Schedule</h4>
+        <div className='answer'>
+          <p>Lorem ipsum sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing Necessitatibus,
+            neque.
+          </p>
+        </div>
+        <div className='faq-line'></div>
+      </div>
+      <div className='question'>
+        <h4>Different Payment Methods</h4>
+        <div className='answer'>
+          <p>Lorem ipsum sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing Necessitatibus,
+            neque.
+          </p>
+        </div>
+        <div className='faq-line'></div>
+      </div>
+      <div className='question'>
+        <h4>What Products do you offer.</h4>
+        <div className='answer'>
+          <p>Lorem ipsum sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing Necessitatibus,
+            neque.
+          </p>
+        </div>
+        <div className='faq-line'></div>
+      </div>
+    </Faq>
+  );
+};
+
+export default FaqSection;
