08f0fab989eff9bafb67cb5c55af01b929aa6cf3
Francis Lagares <francis.lagares@gmail.com>
2021-03-04 19:56:14 +0100

    feat: create nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 
 const App = (): JSX.Element => {
   return (
     <div className='app'>
       <GlobalStyle />
-      <Nav />
-      <AboutUs />
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path='/' exact>
+            <AboutUs />
+          </Route>
+          <Route path='/work'>
+            <OurWork />
+          </Route>
+          <Route path='/contact'>
+            <ContactUs />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
 const StyledNav = styled.nav`
@@ -36,17 +37,19 @@ const Nav = (): JSX.Element => {
   return (
     <StyledNav>
       <h1>
-        <a href='#'>Capture</a>
+        <Link id='logo' to='/'>
+          Capture
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='#'>1. About Us</a>
+          <Link to='/'>1. About Us</Link>
         </li>
         <li>
-          <a href='#'>2. Our Work</a>
+          <Link to='/work'>2. Our Work</Link>
         </li>
         <li>
-          <a href='#'>3. Contact Us</a>
+          <Link to='/contact'>3. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
