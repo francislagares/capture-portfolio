0eee5737aacd94965b0d0f26f82ab375059c71dc
Francis Lagares <francis.lagares@gmail.com>
2021-03-05 19:55:20 +0100

    feat: add link urls

--- src/pages/OurWork.tsx
+++ src/pages/OurWork.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import athlete from '../img/athlete-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 import theracer from '../img/theracer-small.png';
 
 const Work = styled.div`
+  background: white;
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
-  background: white;
 
   h2 {
     padding: 1rem 0;
@@ -20,10 +20,10 @@ const Movie = styled.div`
   padding-bottom: 10rem;
 
   .line {
-    /* prettier-ignore */
-
-    height: .5rem;
     background: #ccc;
+
+    /* prettier-ignore */
+    height: .5rem;
     margin-bottom: 3rem;
   }
 
@@ -40,22 +40,22 @@ const OurWork = (): JSX.Element => {
       <Movie>
         <h2>The Athlete</h2>
         <div className='line'></div>
-        <Link>
+        <Link to='/work/the-athlete'>
           <img src={athlete} alt='Athelete' />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className='line'></div>
-        <Link>
+        <Link to='/work/the-racer'>
           <img src={theracer} alt='Racer' />
         </Link>
       </Movie>
       <Movie>
         <h2>Good Times</h2>
         <div className='line'></div>
-        <Link>
-          <img src={goodtimes} alt='Athelete' />
+        <Link to='/work/good-times'>
+          <img src={goodtimes} alt='Good Times' />
         </Link>
       </Movie>
     </Work>
